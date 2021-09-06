c3450590dea56e35a2b8c8eac0d68e8159122e3a
cafeway <bcaranja@gmai.com>
2021-09-06 08:12:33 -0400

    ..

--- config/app.php
+++ config/app.php
@@ -176,7 +176,18 @@ return [
         App\Providers\RouteServiceProvider::class,
 
     ],
+    /*
+    |--------------------------------------------------------------------------
+    | Application Debug Mode
+    |--------------------------------------------------------------------------
+    |
+    | When your application is in debug mode, detailed error messages with
+    | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
+    |
+    */
 
+    'debug' => env('APP_DEBUG', true),
     /*
     |--------------------------------------------------------------------------
     | Class Aliases
